f2e068696a736e5e15289493f06d11079348330c
Victor Glindås <glinkis@gmail.com>
2017-05-13 20:33:17 +0200

    Sets up a test environment with Mocha and Chai.

---
+++ test/animation/easing.test.js
@@ -0,0 +1,12 @@
+import * as mocha from "mocha";
+import * as chai from "chai";
+import * as easing from "../../src/animation/easing";
+
+const { describe, it } = mocha;
+const { expect, assert } = chai;
+
+describe("Test 1", () => {
+  it("Expects something to be true", () => {
+    assert(true);
+  })
+});
