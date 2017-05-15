6246d39016923208973f010f9ca4c11fdb72f92b
Victor Glindås <glinkis@gmail.com>
2017-05-15 16:51:40 +0200

    Adds more tests.

---
+++ test/dom/getElementAbsolutePosition.test.js
@@ -0,0 +1,14 @@
+import * as chai from 'chai';
+import { getElementAbsolutePosition } from '../../src/dom/getElementAbsolutePosition';
+
+const { expect } = chai;
+
+describe('getElementAbsolutePosition', () => {
+
+  it('is a function', () => {
+    expect(getElementAbsolutePosition).to.be.a('function');
+  });
+
+  // TODO: More extensive tests.
+
+});
