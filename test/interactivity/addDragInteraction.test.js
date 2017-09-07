0d6b85ca4422a707090bf7647f5c6e78ae0391cc
Victor Glindås <glinkis@gmail.com>
2017-09-07 10:25:05 +0200

    Adds more tests to interactivity/addDragInteraction.

--- test/interactivity/addDragInteraction.test.js
+++ test/interactivity/addDragInteraction.test.js
@@ -1,6 +1,9 @@
 import { expect } from "chai";
+import 'phantomjs-polyfill-find';
 import { addDragInteraction } from "../../src/interactivity/addDragInteraction";
-import "../testutils/trackEventListeners";
+import { trackEventListeners } from '../testutils/trackEventListeners';
+
+trackEventListeners();
 
 describe("logic/addDragInteraction", () => {
   it("is a function", () => {
@@ -17,5 +20,13 @@ describe("logic/addDragInteraction", () => {
     );
   });
 
-  // TODO: Check events.
+  it("adds the required events", () => {
+    const listeners = document.body.eventListeners;
+
+    const mousedown = listeners.find(listener => listener.type === 'mousedown');
+    expect(mousedown.listener).to.be.a("function");
+
+    const touchstart = listeners.find(listener => listener.type === 'touchstart');
+    expect(touchstart.listener).to.be.a("function");
+  });
 });
--- test/testutils/trackEventListeners.js
+++ test/testutils/trackEventListeners.js
@@ -1,14 +1,19 @@
 const nativeMethod = HTMLElement.prototype.addEventListener;
 
-
-HTMLElement.prototype.addEventListener = function(type, listener) {
+function newAddEventListener(type, listener) {
   if (!(this.eventListeners instanceof Array)) {
     /** @type {Array<TmpEventListener>} */
     this.eventListeners = [];
   }
   this.eventListeners.push({ type, listener });
   nativeMethod.call(this, type, listener);
-};
+}
+
+export function trackEventListeners() {
+  if (!(HTMLElement.prototype.addEventListener instanceof newAddEventListener)) {
+    HTMLElement.prototype.addEventListener = newAddEventListener;
+  }
+}
 
 /**
  * @typedef {object} TmpEventListener
