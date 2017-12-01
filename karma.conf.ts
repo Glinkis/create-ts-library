e2237069e290abc82e871ea0b382f23fe8a33e60
Victor Glindås <glinkis@gmail.com>
2017-12-01 20:38:39 +0100

    Tries to make tests work again.

--- karma.conf.ts
+++ karma.conf.ts
@@ -2,12 +2,9 @@ module.exports = (config: any) => {
   config.set({
     browsers: [/*"IE", "Chrome", "Firefox", "Safari",*/ "PhantomJS"],
     files: ["src/**/*.ts", "test/**/*.ts"],
-    frameworks: ["mocha", "chai", "karma-typescript"],
-    karmaTypescriptConfig: {
-      tsconfig: "./tsconfig.json"
-    },
+    frameworks: ["mocha", "chai"],
     plugins: [
-      "karma-typescript",
+      "karma-webpack",
       "karma-mocha",
       "karma-chai",
       "karma-phantomjs-launcher"
@@ -17,9 +14,24 @@ module.exports = (config: any) => {
       /*"karma-ie-launcher"*/
     ],
     preprocessors: {
-      "src/**/*.ts": ["karma-typescript"],
-      "test/**/*.ts": ["karma-typescript"]
+      "test/**/*.js": ["webpack"],
+      "test/**/*.ts": ["webpack"]
     },
-    reporters: ["progress", "karma-typescript"]
+    reporters: ["progress"],
+    webpack: {
+      devtool: "inline-source-map",
+      module: {
+        rules: [
+          {
+            exclude: /node_modules/,
+            loader: "ts-loader",
+            test: /\.(js|ts)/
+          }
+        ]
+      },
+      resolve: {
+        extensions: [".ts"]
+      }
+    }
   });
 };
