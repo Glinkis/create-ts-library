9ffe5d7eb5a6b3b199554bf2ddae4a7793b2fc5a
Victor Glindås <glinkis@gmail.com>
2017-12-08 19:10:50 +0100

    Refactors dom tests.

--- test/dom/getElementAbsolutePosition.test.ts
+++ test/dom/getElementAbsolutePosition.test.ts
@@ -4,28 +4,32 @@ import { getElementAbsolutePosition } from "../../src/dom/getElementAbsolutePosi
 import { addOffsetElement } from "../testutils/addOffsetElement";
 import { resetDocument } from "../testutils/resetDocument";
 
-describe("dom/getElementAbsolutePosition", () => {
+function buildDom() {
   resetDocument();
   const element1 = addOffsetElement(document.body, 0, 0, 0, 0);
+  const element2 = addOffsetElement(element1, 10, 20, 0, 0);
+  const element3 = addOffsetElement(element2, 5, 10, 0, 0);
+  return { element1, element2, element3 };
+}
 
-  it("element1 position", () => {
-    const pos = getElementAbsolutePosition(element1);
+describe("dom/getElementAbsolutePosition", () => {
+  it("gets the position of element1", () => {
+    const dom = buildDom();
+    const pos = getElementAbsolutePosition(dom.element1);
     expect(pos.top).to.equal(0);
     expect(pos.left).to.equal(0);
   });
 
-  const element2 = addOffsetElement(element1, 10, 20, 0, 0);
-
-  it("element2 position", () => {
-    const pos = getElementAbsolutePosition(element2);
+  it("gets the position of element2", () => {
+    const dom = buildDom();
+    const pos = getElementAbsolutePosition(dom.element2);
     expect(pos.top).to.equal(10);
     expect(pos.left).to.equal(20);
   });
 
-  const element3 = addOffsetElement(element2, 5, 10, 0, 0);
-
-  it("element3 position", () => {
-    const pos = getElementAbsolutePosition(element3);
+  it("gets the position of element3", () => {
+    const dom = buildDom();
+    const pos = getElementAbsolutePosition(dom.element3);
     expect(pos.top).to.equal(15);
     expect(pos.left).to.equal(30);
   });
--- test/dom/getElementPositionInContainer.test.ts
+++ test/dom/getElementPositionInContainer.test.ts
@@ -2,20 +2,27 @@ import { expect } from "chai";
 import * as mocha from "mocha";
 import { getElementPositionInContainer } from "../../src/dom/getElementPositionInContainer";
 import { addOffsetElement } from "../testutils/addOffsetElement";
+import { resetDocument } from "../testutils/resetDocument";
 
-describe("dom/getElementPositionInContainer", () => {
+function buildDom() {
+  resetDocument();
   const parent = addOffsetElement(document.body, 50, 100, 0, 0);
   const child1 = addOffsetElement(parent, 5, 10, 10, 10);
   const child2 = addOffsetElement(parent, 5, 10, 10, 10);
+  return { parent, child1, child2 };
+}
 
-  it("child1 position", () => {
-    const pos = getElementPositionInContainer(child1, parent);
+describe("dom/getElementPositionInContainer", () => {
+  it("gets the position of child1", () => {
+    const dom = buildDom();
+    const pos = getElementPositionInContainer(dom.child1, dom.parent);
     expect(pos.top).to.equal(5);
     expect(pos.left).to.equal(10);
   });
 
-  it("child2 position", () => {
-    const pos = getElementPositionInContainer(child2, parent);
+  it("gets the position of child2", () => {
+    const dom = buildDom();
+    const pos = getElementPositionInContainer(dom.child2, dom.parent);
     expect(pos.top).to.equal(15);
     expect(pos.left).to.equal(10);
   });
