76ea789005d123c669725dab0116238c96e44227
Victor Glindås <glinkis@gmail.com>
2018-01-15 09:14:52 +0100

    Updates dependencies.

--- test/index_test.js
+++ test/index_test.js
@@ -4,7 +4,7 @@
  * any file that ends with test.ts and get its path. By passing in true
  * we say do this recursively
  */
-var testContext = require.context("./", true, /\.test\.ts/);
+var testContext = require.context("./", true, /\.test\.ts$/);
 
 /**
  * Get all the files, for each file, call the context function
