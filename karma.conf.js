e6a017af8ee93ea58cb9f38ae432d7c55885d4c4
Victor Glindås <glinkis@gmail.com>
2018-03-07 19:18:40 +0100

    Cleans up karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,29 @@
 const webpack = require("webpack");
 
-/**
- * Webpack Configuration
- */
+const sourceMapLoader = {
+  enforce: "pre",
+  test: /\.js$/,
+  loader: "source-map-loader",
+  exclude: /node_modules/
+};
+
+const typeScriptLoader = {
+  test: /\.ts$/,
+  loader: "ts-loader",
+  exclude: /node_modules/
+};
+
 const webpackConfig = {
   mode: "development",
   /* Use this to get nice stack traces. */
   devtool: "eval",
   /* Use this to debug in browser. */
   // devtool: "eval-source-map",
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.ts$/,
-        loader: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
+  module: { rules: [sourceMapLoader, typeScriptLoader] },
   resolve: { extensions: [".js", ".ts"] }
 };
 
-/**
- * Karma Configuration
- */
-module.exports = config => {
+module.exports = config =>
   config.set({
     basePath: "",
     plugins: [
@@ -40,20 +33,14 @@ module.exports = config => {
       "karma-chrome-launcher",
       "karma-sourcemap-loader"
     ],
-    browsers: process.env.TRAVIS ? ["Chrome_travis_ci"] : ["ChromeHeadless"],
+    browsers: process.env.TRAVIS ? ["ChromeTravis"] : ["ChromeHeadless"],
     customLaunchers: {
-      Chrome_travis_ci: { base: "Chrome", flags: ["--no-sandbox"] }
+      ChromeTravis: { base: "Chrome", flags: ["--no-sandbox"] }
     },
     files: [{ pattern: "./test/index.js", watched: false }],
     preprocessors: { "./test/index.js": ["webpack", "sourcemap"] },
     frameworks: ["mocha", "chai"],
     reporters: ["progress"],
-    specReporter: {
-      maxLogLines: Infinity, // Limit number of lines logged per test.
-      showSpecTiming: true, // Print the time elapsed for each spec.
-      failFast: true // Test would finish with error when a first fail occurs.
-    },
     webpack: webpackConfig,
     webpackMiddleware: { noInfo: true }
   });
-};
