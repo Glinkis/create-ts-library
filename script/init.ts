2dc6e2c6ae74672d515aa758a9f365ca846c2f2a
Victor Glindås <glinkis@gmail.com>
2018-11-29 13:13:17 +0100

    Add src & test to init command.

--- script/init.ts
+++ script/init.ts
@@ -16,6 +16,26 @@ export const init = () => {
     fs.writeFileSync(tslintPath, tslintTemplate, "utf8");
     success("Created tslint.json");
   }
+
+  const srcDir = `${dir}/src`;
+  const srcPath = `${srcDir}/index.ts`;
+  if (!fs.existsSync(srcPath)) {
+    if (!fs.existsSync(srcDir)) {
+      fs.mkdirSync(srcDir);
+    }
+    fs.writeFileSync(srcPath, "", "utf8");
+    success("Created src/index.ts");
+  }
+
+  const testDir = `${dir}/test`;
+  const testPath = `${testDir}/index.test.ts`;
+  if (!fs.existsSync(testPath)) {
+    if (!fs.existsSync(testDir)) {
+      fs.mkdirSync(testDir);
+    }
+    fs.writeFileSync(testPath, "", "utf8");
+    success("Created test/index.test.ts");
+  }
 };
 
 const tsconfigTemplate = `{
