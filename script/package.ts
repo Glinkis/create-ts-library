66f5a3f9b9870e1631013170353b5b8f8dfc01b7
Victor Glindås <glinkis@gmail.com>
2018-11-30 09:23:44 +0100

    Allow specifying the library global.

--- script/package.ts
+++ script/package.ts
@@ -5,15 +5,34 @@ const pack = JSON.parse(
   fs.readFileSync(`${process.cwd()}/package.json`, "utf8"),
 );
 
-if (!pack.name || typeof pack.name !== "string") {
+if (!pack.name) {
   throw abort("Could not find 'name' in package.json");
+} else if (typeof pack.name !== "string") {
+  throw abort("package.json: name is not a string.");
 }
 
-if (!pack.version || typeof pack.version !== "string") {
+if (!pack.version) {
   throw abort("Could not find 'version' in package.json");
+} else if (typeof pack.version !== "string") {
+  throw abort("package.json: config is not a string.");
+}
+
+if (!pack.config) {
+  throw abort("Could not find 'config' in package.json");
+} else if (typeof pack.config !== "object") {
+  throw abort("package.json: config is not an object.");
+}
+
+if (!pack.config.library) {
+  throw abort("Could not find 'config/library' in package.json");
+} else if (typeof pack.config.library !== "string") {
+  throw abort("package.json: config/library is not a string.");
 }
 
 export default pack as {
   name: string;
   version: string;
+  config: {
+    library: string;
+  };
 };
--- script/webpack/webpack.config.ts
+++ script/webpack/webpack.config.ts
@@ -7,14 +7,19 @@ const dir = process.cwd();
 
 export default {
   devtool: "source-map",
+  // Since we're not using a typescript loader, the entry has
+  // to be the directory that the source is compiled to.
   entry: path.resolve(dir, "dist/es"),
   output: {
-    library: "sample", // library API name
-    libraryTarget: "umd", // universal module definition
+    // Library API name.
+    library: pack.config.library,
+    // Universal module definition.
+    libraryTarget: "umd",
   },
   plugins: [
+    // Bundles the type definitions into a single file.
     new DtsBundlePlugin({
-      name: pack.name,
+      name: pack.config.library,
       main: path.resolve(dir, "dist/types/index.d.ts"),
       out: path.resolve(dir, `dist/${pack.name}.${pack.version}.d.ts`),
       removeSource: false,
